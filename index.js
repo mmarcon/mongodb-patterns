d9d7e00d8554b645859c46994db6e2eb51256616
Massimiliano Marcon <emaildelmax@gmail.com>
2022-09-12 06:24:27 +0000

    Print random data modeling pattern

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import * as dotenv from 'dotenv';
+import chalk from 'chalk';
 
 dotenv.config();
 
 const uri = process.env.MONGODB_CONNECTION_STRING;
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+const DB = 'mongodb_patterns';
+const COLLECTION = 'patterns';
 
 async function go () {
   try {
     await client.connect();
-    await client.db('admin').command({ ping: 1 });
-    console.log('Connected successfully to server');
+    const coll = client.db(DB).collection(COLLECTION);
+    const randomPatterns = coll.aggregate([
+      { $sample: { size: 1 } }
+    ]);
+    for await (const pattern of randomPatterns) {
+      console.log('---');
+      console.log(chalk.blue(pattern.name));
+      console.log(pattern.description);
+      if (pattern.pros.length > 0) {
+        console.log(chalk.green.bold('\nPros'));
+        pattern.pros.forEach(p => console.log(chalk.green(`+ ${p}`)));
+      }
+      if (pattern.cons.length > 0) {
+        console.log(chalk.red.bold('\nCons'));
+        pattern.cons.forEach(c => console.log(chalk.red(`- ${c}`)));
+      }
+      if (pattern.notes.length > 0) {
+        console.log(`\n${chalk.bold('Notes:')} ${pattern.notes}`);
+      }
+      console.log('---');
+    }
   } finally {
     await client.close();
   }
