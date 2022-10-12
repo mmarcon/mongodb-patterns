2bed99b89402f7967588eee8816d13eeab4f735c
Massimiliano Marcon <emaildelmax@gmail.com>
2022-10-12 08:32:06 +0000

    Added $search fuzzy query

--- index.js
+++ index.js
@@ -13,10 +13,41 @@ async function go () {
   try {
     await client.connect();
     const coll = client.db(DB).collection(COLLECTION);
+    console.log(chalk.bold('\nRandom Data Modeling Pattern'));
     const randomPatterns = coll.aggregate([
       { $sample: { size: 1 } }
     ]);
     for await (const pattern of randomPatterns) {
+      console.log('---');
+      console.log(chalk.blue(pattern.name));
+      console.log(pattern.description);
+      if (pattern.pros.length > 0) {
+        console.log(chalk.green.bold('\nPros'));
+        pattern.pros.forEach(p => console.log(chalk.green(`+ ${p}`)));
+      }
+      if (pattern.cons.length > 0) {
+        console.log(chalk.red.bold('\nCons'));
+        pattern.cons.forEach(c => console.log(chalk.red(`- ${c}`)));
+      }
+      if (pattern.notes.length > 0) {
+        console.log(`\n${chalk.bold('Notes:')} ${pattern.notes}`);
+      }
+      console.log('---');
+    }
+    console.log(chalk.bold('\nAll patterns related to versioning'));
+    const versioningPatterns = coll.aggregate([{
+      $search: {
+        index: 'patterns-search-index',
+        text: {
+          query: 'version',
+          path: {
+            wildcard: '*'
+          },
+          fuzzy: {}
+        }
+      }
+    }]);
+    for await (const pattern of versioningPatterns) {
       console.log('---');
       console.log(chalk.blue(pattern.name));
       console.log(pattern.description);
--- patterns/_load.js
+++ patterns/_load.js
@@ -4,10 +4,13 @@
 const DB = 'mongodb_patterns';
 const COLLECTION = 'patterns';
 
+// Drop collection if it exists
+db.getSiblingDB(DB).getCollection(COLLECTION).drop();
+
 // List JSON files
 console.log(`Loading files in ${__dirname}`);
 const files = fs.readdirSync(__dirname);
-for (const file of files.filter(f => /^.+\.json$/.test(f))) {
+for (const file of files.filter(f => /^[^_].+\.json$/.test(f))) {
   const filePath = path.join(__dirname, file);
   console.log(`Reading and parsing ${filePath}`);
   const pattern = EJSON.parse(fs.readFileSync(filePath));
